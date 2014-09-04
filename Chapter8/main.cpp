880e982ee03c166930f124b9fcad513443b4d13e
Yu Chen <yu.chen@phinarys.com>
2014-09-04 21:26:49 +0800

    Added more Chapter 8

--- Chapter8/main.cpp
+++ Chapter8/main.cpp
@@ -10,6 +10,39 @@
 
 using namespace std;
 
+// 8.3
+bool f1(){
+    cout << "f1" << endl;
+    return false;
+}
+bool f2(){
+    cout << "f2" << endl;
+    return true;
+}
+class V {
+public:
+    int vec[2];
+    V(int a0, int a1) { vec[0]=a0; vec[1]=a1; }
+    bool operator&&(V &arg) {
+        return abs(vec[0]) + abs(vec[1]) > 0 && 
+               abs(arg.vec[0]) + abs(arg.vec[1]) > 0;
+    }
+};
+bool operator!(V &v) {
+    return v.vec[0] * v.vec[1] != 0;
+}
+
+// 8.4
+class Fun {
+public:
+    int operator() (int a1, int a2) {
+        return a1 > a2 ? a1 : a2;
+    }
+    int operator() (int a1, int a2, int a3) {
+        return a1 > a2 ? (a1 > a3 ? a1 : a3) : (a2 > a3 ? a2 : a3);
+    }
+};
+
 /*
  * 
  */
@@ -27,13 +60,26 @@ int main(int argc, char** argv) {
     */
     
     // 8.2
+    /*
     enum weekday {SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY};
     int day = SUNDAY;
     // weekday day1 = 0; // error: invalid conversion from ‘int’ to ‘main(int, char**)::weekday’ [-fpermissive]
     cout << day << endl;
-    
     enum Symbols {ALPHA = -1, BETA = -2, GEMMA};
+    */
     
+    // 8.3
+    if(f1() && f2()){ // f1 only
+    }
+    V v1(4, 8), v2(3, 7), v3(0, 0), v4(0, 0);
+    cout << (v1 && v2 ? "true" : "false") << endl;
+    // cout << (v3 && v4 && v1 ? "true" : "false") << endl; // error: no match for ‘operator&&’ in ‘v3.V::operator&&((* & v4)) && v1’
+    cout << (!v1 ? "true" : "false") << endl;
+    
+    // 8.4
+    Fun f; // Functor
+    cout << f(1,2) << endl;
+    cout << f(1,2,3) << endl;
     
     return 0;
 }
